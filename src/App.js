ffc96e7d6e58d9575d45c21c4d10d9037cb438be
Saif Ur Rehman <saifurrehman.web@gmail.com>
2023-08-24 19:04:12 +0500

    Todo Item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './Todo.scss'
+import React from 'react';
 function App() {
+  const[Todoarr ,setTodoarr] = React.useState([])
+  const [list, setlist]=React.useState()
+  const change =(e)=>{
+    setlist(e.target.value)
+  }
+  const add =()=>{
+    if(list===""){
+      return;
+
+    }   
+    if(Todoarr.length>10){
+      alert("You Cant able to add more Todo's For Now")
+    }
+    else{
+      let valueof = {text:list,key:Date.now()}
+      setTodoarr([...Todoarr,valueof])
+      setlist("")
+    }  
+  }
+  const clickDEl=(itemdel)=>{
+    const pro = Todoarr.filter((item)=>{
+     return( item.key !== itemdel.key
+     ) })
+  setTodoarr(pro)
+  }
+  // const clickDEl = (itemToDelete) => {
+  //   const updatedTodoArr = Todoarr.filter((item) => {
+  //     return item.key !== itemToDelete.key;
+  //   });
+  
+  //   setTodoarr(updatedTodoArr);
+  // };
+  // console.log(Todoarr)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className="main">
+
+    <div className="Todo">
+      <input type="text" value={list} onChange={change} placeholder='Write Todo' />
+    <button className='first' onClick={add}>+</button>
+    <ul>
+      {
+        Todoarr.map((item)=>{
+          return(
+            <div style={{display:"flex",justifyContent:"space-between",padding:"0rem 1rem"}}>
+               <li key={item.key} >{item.text}</li>
+               <button onClick={()=>clickDEl(item)} className="cancel-button">
+    <span className="cancel-icon">X</span>
+  </button>
+            </div>
+         
+          )
+        })
+      }
+    </ul>
     </div>
+
+    </section>
+
+
   );
 }
 
